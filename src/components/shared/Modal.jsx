b1872f19fb1d3e0a338b7bf1f3a720a7171950c8
Mike Mitchell <mjmitchelldpt1@gmail.com>
2022-12-20 13:58:44 -0600

    delete modal styling complete

--- src/components/shared/Modal.jsx
+++ src/components/shared/Modal.jsx
@@ -3,29 +3,31 @@ import Button from "./Button";
 
 const Modal = ({ setModal }) => {
   return (
-    <div id="modalBackground" className="">
-      <div id="modalContainer" className="">
-        <div id="title" className="">
-          <h2>Continue?</h2>
-        </div>
-        <div id="body" className="">
-          <p>This will permanently delete this entry!</p>
-        </div>
-        <div id="footer" className="">
-          <button
-            className="button-primary"
-            onClick={() =>
-              setModal({ isModalOpen: false, confirmDelete: true })
-            }
-          >
-            Confirm
-          </button>
-          <button
-            className="button-primary"
-            onClick={() => setModal({ isModalOpen: false })}
-          >
-            Cancel
-          </button>
+    <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-brightness-50">
+      <div className="relative w-auto my-6 mx-auto max-w-3xl ">
+        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none">
+          <div className="flex items-start justify-between p-5 border-b border-solid border-white rounded-t bg-black">
+            <h3 className="text-2xl font-semibold">Confirm Delete</h3>
+          </div>
+          <div className="relative p-4 flex-auto bg-slate-500">
+            <p>You gone</p>
+          </div>
+          <div className="flex items-center justify-end p-4 border-t border-solid border-white bg-slate-500 rounded-b">
+            <button
+              className="button-primary"
+              onClick={() =>
+                setModal({ isModalOpen: false, confirmDelete: true })
+              }
+            >
+              Confirm
+            </button>
+            <button
+              className="button-primary hover:bg-red-400"
+              onClick={() => setModal({ isModalOpen: false })}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </div>
