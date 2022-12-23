e11a63d881a970c1888e91fc80ebfbe5acdda3f6
Mike Mitchell <mjmitchelldpt1@gmail.com>
2022-12-23 07:14:23 -0600

    minor fix

--- src/components/ProductivityStats.jsx
+++ src/components/ProductivityStats.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 const ProductivityStats = ({ productivityData }) => {
   const totalAverage = productivityData.reduce((total, item) => {
-    console.log(typeof total, total, "total");
-    console.log(typeof item.rating, item.rating, "rating");
     return total + item.rating;
   }, 0);
 
