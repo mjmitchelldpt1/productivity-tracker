32d173b022772a5ad4882e7628dd47080a2f90e5
Mike Mitchell <mjmitchelldpt1@gmail.com>
2023-01-09 12:58:48 -0600

    env changes

--- src/components/api/config.tsx
+++ src/components/api/config.tsx
@@ -1,3 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const API_BASE_URL = import.meta.env.VITE_LOCAL_API_URL;
+
+//for querying and manage data
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
