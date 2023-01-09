9aee3116bd2d5edd0e61edcf3f5a26120be35f04
Mike Mitchell <mjmitchelldpt1@gmail.com>
2023-01-09 13:11:31 -0600

    change to process.env

--- src/components/api/config.tsx
+++ src/components/api/config.tsx
@@ -3,6 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 export const API_BASE_URL = import.meta.env.VITE_LOCAL_API_URL;
 
 //for querying and manage data
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "dotenv/config";
 
 const queryClient = new QueryClient();
 
