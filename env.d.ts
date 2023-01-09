e1087411e53c1a1d583c1edb54aa2faa72a2fac6
mjmitchelldpt1 <95656749+mjmitchelldpt1@users.noreply.github.com>
2023-01-09 12:32:17 -0600

    Delete env.d.ts

--- env.d.ts
+++
@@ -1,8 +0,0 @@
-///<reference types="vite/client" />
-
-LOCAL_API_URL = "http://localhost:3000";
-
-SUPABASE_URL = "https://hioyopdcoyrcqunbmkfe.supabase.co";
-
-SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhpb3lvcGRjb3lyY3F1bmJta2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMyNzQ0NDksImV4cCI6MTk4ODg1MDQ0OX0.qS4-7xCQEppUTDdP7SEipGw1t1yvNFD__d0mHG9ByUo";
