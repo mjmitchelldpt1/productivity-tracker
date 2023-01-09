30302fc887e9ad641a1300f837ea40b787dc1f42
Mike Mitchell <mjmitchelldpt1@gmail.com>
2023-01-09 13:30:16 -0600

    trial

--- src/components/api/config.ts
+++ src/components/api/config.ts
@@ -4,6 +4,6 @@ import { createClient } from "@supabase/supabase-js";
 export const API_BASE_URL = process.env.LOCAL_API_URL;
 
 //for querying and manage data
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
