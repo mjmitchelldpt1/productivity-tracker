a76d2f6fd89ed613e781209db062afd2edf09feb
Mike Mitchell <mjmitchelldpt1@gmail.com>
2023-01-09 13:01:54 -0600

    testing

--- src/components/api/config.tsx
+++ src/components/api/config.tsx
@@ -4,5 +4,5 @@ export const API_BASE_URL = import.meta.env.VITE_LOCAL_API_URL;
 
 //for querying and manage data
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
